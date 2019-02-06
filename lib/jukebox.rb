575e83adebf6a5de75ce97c6246172263eb9dd6e
Sean Kelly <shonkelli@gmail.com>
2019-02-06 18:40:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,8 +24,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
   song = valid_song(user_input, songs)
-  if 
-    puts "Playing "
+  if song
+    puts "Playing #{song}"
   else 
     puts "Invalid input, please try again."
   end
