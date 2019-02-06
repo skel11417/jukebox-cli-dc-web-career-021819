0e0b804f6c22a4d55285fe3fb030fbf33f5bcc4c
Sean Kelly <shonkelli@gmail.com>
2019-02-06 18:32:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,6 +33,7 @@ end
 def valid_song(input, songs)
   # checks to see if the input is a song and returns the name
   # of the song. otherwise returns false
+  
   if (1..songs.length).include?(input.to_i)
     return true
   else
